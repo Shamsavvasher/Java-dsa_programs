33dbda877d4eff2262e47d27334a263a0513e05d
sham savvasher <yoyoshamsavvasher@gmail.com>
2024-02-06 07:25:03 +0530

    added new file

---
+++ sorting/bubbleSort.java
@@ -0,0 +1,26 @@
+public class bubbleSort {
+
+    public static void bubblesort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 5, 4, 20, 1, 3 };
+        bubblesort(arr);
+        printarr(arr);
+    }
+}
